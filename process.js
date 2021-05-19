bc6df22525f936c39ce451a591db9f11d2d10de0
Tom Dyson <tom@torchbox.com>
2021-05-19 14:54:29 +0100

    Post-process step

---
+++ process.js
@@ -0,0 +1,18 @@
+// Helper library written for useful postprocessing tasks with Flat Data
+// Has helper functions for manipulating csv, txt, json, excel, zip, and image files
+import { readJSON, writeJSON } from "https://deno.land/x/flat@0.0.10/mod.ts";
+
+const json = await readJSON("data.json");
+
+// Step 2: Filter specific data we want to keep and write to a new JSON file
+const metrics = Object.keys(json); // convert property values into an array
+
+const metricArray = metrics.map((metric) => ({
+  metric: metric,
+  value: json[metric],
+}));
+
+// Step 3. Write a new JSON file with our filtered data
+const newFilename = `data-postprocessed.json`; // name of a new file to be saved
+await writeJSON(newFilename, metricArray); // create a new JSON file with just the Bitcoin price
+console.log("Wrote a post process file");
